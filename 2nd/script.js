6f602eabad79daea9195b5cd2d12b4473962bcfb
mkashaf <2021i0346@gmail.com>
2025-10-17 14:51:55 +0500

    download progress bar using js

--- 2nd/script.js
+++ 2nd/script.js
@@ -23,40 +23,64 @@
 // })
 
 
-// Form Validation 
-let form = document.querySelector("#loginForm");
-let email = document.querySelector("#email");
-let pass = document.querySelector("#password")
-form.addEventListener("submit", function(dets){
-    dets.preventDefault();
-    console.log(email.value);
-    console.log(pass.value);
+// // Form Validation 
+// let form = document.querySelector("#loginForm");
+// let email = document.querySelector("#email");
+// let pass = document.querySelector("#password")
+// form.addEventListener("submit", function(dets){
+//     dets.preventDefault();
+//     console.log(email.value);
+//     console.log(pass.value);
 
-    let emailError = document.querySelector("#emailError").textContent="";
-    let passError = document.querySelector("#passwordError").textContent="";
+//     let emailError = document.querySelector("#emailError").textContent="";
+//     let passError = document.querySelector("#passwordError").textContent="";
 
-    const mailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-    const strongPwdRegex = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+//     const mailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+//     const strongPwdRegex = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
 
-    let mailAns = mailRegex.test(email.value);
-    let paswdAns = strongPwdRegex.test(pass.value);
-    
-    isValid = true;
+//     let mailAns = mailRegex.test(email.value);
+//     let paswdAns = strongPwdRegex.test(pass.value);
 
-    if(!mailAns){
-        document.querySelector("#emailError").textContent="Mail is Encorrect";
-        document.querySelector("#emailError").style.display="initial";
-        isValid = false;
+//     isValid = true;
+
+//     if(!mailAns){
+//         document.querySelector("#emailError").textContent="Mail is Encorrect";
+//         document.querySelector("#emailError").style.display="initial";
+//         isValid = false;
+//     }
+//     if (!paswdAns) {
+//         document.querySelector("#passwordError").textContent="Password is Incorrect";
+//         document.querySelector("#passwordError").style.display="initial";
+//         isValid = false;
+//     }
+
+//     if (isValid == true) {
+//         document.querySelector(".result").textContent="EveryThing Is Correct"
+//     }
+
+// })
+
+
+
+let progress = document.querySelector("#progressBar");
+let textprogress = document.querySelector("#progressText");
+let startbtn = document.querySelector("#startBtn");
+let count = 0;
+
+startbtn.addEventListener("click", function() {
+  let interval = setInterval(function() {
+    if (count < 100) {
+      count++;
+      progress.style.width = `${count}%`;
+      textprogress.textContent = `${count}%`;
+      console.log(count);
+    } else {
+      clearInterval(interval);
+      startbtn.textContent="Download Complete";
     }
-    if (!paswdAns) {
-        document.querySelector("#passwordError").textContent="Password is Incorrect";
-        document.querySelector("#passwordError").style.display="initial";
-        isValid = false;
-    }
+  }, 1000); 
+});
 
-    if (isValid == true) {
-        document.querySelector(".result").textContent="EveryThing Is Correct"
-    }
 
-})
+
 
